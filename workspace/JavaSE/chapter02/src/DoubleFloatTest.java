2ec5e24a76cd617cb9ceb53a58f3de14cd5bb49b
铁壁蛤蟆-java <3045546466@qq.com>
2025-12-12 19:57:07 +0800

    changes

--- workspace/JavaSE/chapter02/src/DoubleFloatTest.java
+++ workspace/JavaSE/chapter02/src/DoubleFloatTest.java
@@ -1,11 +1,10 @@
-class DoubleFloatTest 
+class DoubleFloatTest
 {
 	public static void main(String[] args) 
 	{
 		//测试1：（解释见章末企业真题：为什么0.1 + 0.2不等于0.3）
 		System.out.println(0.1 + 0.2);
 		//0.30000000000000004
-
 		//测试2：
 		float ff1 = 123123123f;
 		//float只能精确表示七位以内的计算
