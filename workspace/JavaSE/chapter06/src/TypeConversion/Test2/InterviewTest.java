fd6a2d64c6cdbfcf99b669809d00149c515348db
铁壁蛤蟆-java <3045546466@qq.com>
2025-12-18 11:58:14 +0800

    changes

---
+++ workspace/JavaSE/chapter06/src/TypeConversion/Test2/InterviewTest.java
@@ -0,0 +1,32 @@
+package TypeConversion.Test2;
+
+/**
+ * ClassName: InterviewTest
+ * Package: TypeConversion.Test2
+ * Description:
+ *
+ * @Author Soon
+ * @Create 2025/12/18 11:39
+ * @Version 1.0
+ */
+
+/**
+ * int... 可以兼容int[]
+ * 但是int[]不可以兼容int...
+ *
+ */
+public class InterviewTest {
+
+    public static void main(String[] args) {
+        Base base = new Sub();
+        //通过base引用调用可以传数组或可变参数
+        base.add(1,2,3);
+        base.add(1,new int[2]);
+
+
+
+        //通过base引用调用只能传数组
+        //base.sum(1,2,3);
+        base.sum(1,new int[2]);
+    }
+}
